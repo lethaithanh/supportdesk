19a0c2c0bdec2beb0f932f9e26eabf5543491f8f
Le Thai Thanh <admin@lethai.vn>
2022-06-16 22:46:10 +0700

    Commit on 16-06-2022

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,28 @@
-console.log('Server ...');
+const express = require('express');
+const colors = require('colors');
+const dotenv = require('dotenv').config();
+const { errorHandle } = require('./middleware/errorMiddleware');
+const connectDB = require('./config/db');
+
+// Connect to database
+connectDB();
+
+// Express App
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Welcome to the Support Desk API' });
+});
+
+// Routes
+app.use('/api/users', require('./routes/userRoutes'));
+
+// Error Handle
+app.use(errorHandle);
+
+// Port
+const port = process.env.PORT || 8000;
+app.listen(port, () => console.log(`Server started on port: ${port}`));
